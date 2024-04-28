373b9cf9d9e049c9b4e118783c96089bbfa3fa19
Suraj Yadav <ysuraj877@gmail.com>
2024-04-28 13:32:58 +0530

    Read from keyboard and print

--- main.c
+++ main.c
@@ -1,8 +1,40 @@
 #include "main.h"
 #include <stdio.h>
+#include <fcntl.h>
+#include <linux/input.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#if !defined(BUFFER_SIZE)
+#define BUFFER_SIZE 1024
+#endif
+
+// Errors
+#define EOPEN 1
+#define EREAD 2
 
 static char *KEYBOARD_DEVICE = KEYBOARD_EVENT_PATH;
+static char BUFFER[BUFFER_SIZE];
 
-int main(){
-    printf("%s", KEYBOARD_DEVICE);
+int main()
+{
+    int fkeyboard_device = open(KEYBOARD_DEVICE, O_RDONLY, NULL);
+    if (fkeyboard_device < 0)
+    {
+        exit(EOPEN);
+    }
+
+    struct input_event event;
+    while (1)
+    {
+        int read_inputs = read(fkeyboard_device, &event, sizeof(struct input_event));
+        if (read_inputs < 0)
+        {
+            exit(EREAD);
+        }
+        if (event.type == EV_KEY)
+        {
+            printf("Pressed %d", event.code);
+        }
+    }
 }
